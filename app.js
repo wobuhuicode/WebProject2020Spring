e61bd8466ce1937f6246361ac3b83dd3c6560cc7
Kanglun Zhao <wobuhuicode@outlook.com>
2020-06-02 13:04:17 +0800

    Login Page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/login');
 var usersRouter = require('./routes/users');
 
 var app = express();
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IGC' });
+  res.render('login', { title: 'IGC' });
 });
 
 module.exports = router;
