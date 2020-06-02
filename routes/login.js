83ab9f3bbec0ebeb099cc88e76a1c42339b79184
Kanglun Zhao <wobuhuicode@outlook.com>
2020-06-03 00:33:15 +0800

    Search & Rank 页面

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,72 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  res.render('rank_search', { title: 'IGC' });
+  res.render('rank_search', { title: 'IGC', username: username, game: game});
 });
+
+//example data
+var username = 'yang xinqi';
+var game = {
+  t1: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t2: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t3: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t4: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t5: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t6: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t7: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t8: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t9: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+  t10: {
+    title: "League of Legends",
+    score: 9.5,
+    type: 'MOBA',
+    link: '/game?=lol'
+  },
+}
 
 module.exports = router;
