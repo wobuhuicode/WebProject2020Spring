b4be9825885f6b8f842dd7c58849f76250573970
16342 <16342@DESKTOP-6DH34HG>
2020-06-02 21:05:56 +0800

    sql

--- routes/SQL.js
+++ routes/SQL.js
@@ -1,1 +1,14 @@
-// JavaScript source code
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'yourpassword',
+    database: 'sys'
+});
+connection.connect();
+
+module.exports = {
+    connection,
+};
