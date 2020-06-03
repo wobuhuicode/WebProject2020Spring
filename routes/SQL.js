362aa2faccf3c3cc71bf37444c904797205a7fd3
YiLei Liu <51941435+Calling-Liu@users.noreply.github.com>
2020-06-03 17:51:25 +0800

    更新一些sql操作

--- routes/SQL.js
+++ routes/SQL.js
@@ -1,7 +1,95 @@
 var mysql = require('mysql');
 
-
-
+//初始化页面
+function information(callback)
+{
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('SELECT game_id,name,star from game order by star desc', function (error, results, fields) {
+        if (error) throw error;
+        con.end();
+        callback(results);
+    });
+}
+//通过姓名查询信息
+function get_information(id,callback)
+{
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('SELECT * from game where name = "' + id + '"', function (error, results, fields) {
+        if (error) throw error;
+        con.end();
+        callback(results);
+    });
+}
+//通过姓名查询信息
+function get_comment_information(name,callback)
+{
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('SELECT comment,user_id from comments where game_name = "' + name + '"', function (error, results, fields) {
+        if (error) throw error;
+        con.end();
+        callback(results);
+    });
+}
+//通过id查询信息
+function get_id_information(id,callback)
+{
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('SELECT * from game where game_id = ' + id , function (error, results, fields) {
+        if (error) throw error;
+        con.end();
+        callback(results);
+    });
+}
+//验证姓名是否存在
+function exist(name, callback)
+{
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('select * from game where name = "' + name + '"', function (error, result,fields) {
+        if (error) throw error;
+        if(result.length != 0){
+            con.end();
+            callback(1);
+        }else {
+            con.end();
+            callback(-1);
+        }
+    });
+}
 //修改评分
 function update_star(game_id,game_name,user_id, comment,score,callback){
     var con = mysql.createConnection({
@@ -52,9 +140,51 @@ function set_new_star(game_id,score,callback){
         callback(result);
     });
 }
-
-module.exports = {
+//确定用户是否已经评论过了
+function exist_comment(game_id, user_id, callback){
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('select * from comments where game_id = ? and user_id = ?', [game_id, user_id],function (error, result,fields) {
+        if (error) throw error;
+        if(result.length != 0){
+            con.end();
+            callback(1);
+        }else {
+            con.end();
+            callback(-1);
+        }
+    });
+}
+//更新评分和自己的评论
+function update_comment(game_id,game_name,user_id, comment,score,callback){
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: '3306',
+        password: '1234',
+        database: 'igc'
+    });
+    con.connect();
+    con.query('update comments set comment = ? , star = ? where game_id = ? and user_id = ?', [comment, score, game_id,  user_id],function (error, result,fields) {
+        if (error) throw error;
+        con.end();
+        callback(result);
+    });
+}
+module.exports={
+    get_information,
+    exist,
+    information,
+    get_id_information,
+    get_comment_information,
     update_star,
     update_now,
-    set_new_star
+    exist_comment,
+    update_comment
 };
